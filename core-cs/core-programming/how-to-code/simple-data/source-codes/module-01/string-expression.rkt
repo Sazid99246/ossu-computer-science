a9430eae554b411f014c8c5cb949ddf2179bd619
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2023-01-28 22:31:26 +0600

    added some codes of the module-1A of how-code-simple data

---
+++ core-cs/core-programming/how-to-code/simple-data/source-codes/module-01/string-expression.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname string-expression) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+"apple"
+"Sazid"
+(string-append "Ada" " " "Loveless")
+(string-length "apple")
+(substring "Caribou" 2 4)
+(substring "0123456789" 2 4)
+(substring "Caribou" 0 3)
