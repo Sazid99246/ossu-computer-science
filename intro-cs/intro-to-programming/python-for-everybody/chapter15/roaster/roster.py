b11a35b684236e63d07ee5dfec70a097b1c8e6ae
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2023-02-03 17:18:27 +0600

    Done with htdd design structure

--- intro-cs/intro-to-programming/python-for-everybody/chapter15/roaster/roster.py
+++ intro-cs/intro-to-programming/python-for-everybody/chapter15/roaster/roster.py
@@ -9,17 +9,14 @@ cur.executescript('''
 DROP TABLE IF EXISTS User;
 DROP TABLE IF EXISTS Member;
 DROP TABLE IF EXISTS Course;
-
 CREATE TABLE User (
     id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     name   TEXT UNIQUE
 );
-
 CREATE TABLE Course (
     id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     title  TEXT UNIQUE
 );
-
 CREATE TABLE Member (
     user_id     INTEGER,
     course_id   INTEGER,
@@ -28,10 +25,7 @@ CREATE TABLE Member (
 )
 ''')
 
-fname = './roster_data.json'
-if len(fname) < 1:
-    fname = 'roaster_data.json'
-
+fname = 'assets/py4e/roster_data.json'
 # [
 #   [ "Charley", "si110", 1 ],
 #   [ "Mea", "si110", 0 ],
@@ -45,20 +39,20 @@ for entry in json_data:
     title = entry[1];
     role = entry[2];
 
-    print((name, title, role))
+    print(name, title, role)
 
-    cur.execute('''INSERT OR IGNORE INTO User (name)
+    cur.execute('''INSERT OR IGNORE INTO User (name) 
         VALUES ( ? )''', ( name, ) )
     cur.execute('SELECT id FROM User WHERE name = ? ', (name, ))
     user_id = cur.fetchone()[0]
 
-    cur.execute('''INSERT OR IGNORE INTO Course (title)
+    cur.execute('''INSERT OR IGNORE INTO Course (title) 
         VALUES ( ? )''', ( title, ) )
     cur.execute('SELECT id FROM Course WHERE title = ? ', (title, ))
     course_id = cur.fetchone()[0]
 
     cur.execute('''INSERT OR REPLACE INTO Member
-        (user_id, course_id, role) VALUES ( ?, ?, ? )''',
+        (user_id, course_id, role) VALUES ( ?, ?, ? )''', 
         ( user_id, course_id, role ) )
 
     conn.commit()
