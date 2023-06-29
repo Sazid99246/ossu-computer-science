7eb7761946a21eb4c968eebf2f24a89042af00a0
Sheikh Md. Sazidul Islam <sheikhmdsazidulislam@gmail.com>
2023-06-29 10:53:49 +0600

    python_for_everybody chapter_02 completed

---
+++ python_for_everybody/chapter_02/celsius_to_fahrenheit.py
@@ -0,0 +1,4 @@
+celsius = int(input("Enter the temperature in celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+degree_symbol = "\u00b0"
+print(str(fahrenheit) + degree_symbol + "F")
---
+++ python_for_everybody/chapter_02/gross_pay.py
@@ -0,0 +1,4 @@
+hours = int(input("Enter hours: "))
+rate_per_hour = float(input("Enter rate: "))
+gross_pay = hours * rate_per_hour
+print("Pay: " + str(gross_pay))
